f075a3ea034a4cd6e4390f9d1e59d28502c62d4f
Aaron Kromer <aaron.kromer@gmail.com>
2012-11-20 20:52:47 -0500

    Purge with no rules.

--- lib/purger.rb
+++ lib/purger.rb
@@ -2,4 +2,11 @@ class Purger
   def initialize(directory)
   end
 
+  def purge
+    self
+  end
+
+  def last_purged
+    []
+  end
 end
--- spec/purger_spec.rb
+++ spec/purger_spec.rb
@@ -1,7 +1,21 @@
 require 'spec_helper'
 
 describe Purger do
+  subject(:purger) { Purger.new 'adirectory' }
+
   it '#new requires a directory' do
     expect{ Purger.new }.to raise_error
+  end
+
+  describe '#purge' do
+    it 'returns self' do
+      purger.purge.should be purger
+    end
+
+    context 'no rules provided' do
+      it '#last_purged should be empty' do
+        purger.purge.last_purged.should be_empty
+      end
+    end
   end
 end
